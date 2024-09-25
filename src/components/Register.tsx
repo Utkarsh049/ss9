22aa34ba41b109be804b810b5b5cdd5ed39ee887
Utkarsh049 <utkarshpcs26@gmail.com>
2024-09-25 13:40:30 +0530

    connected to the forms

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -9,7 +9,7 @@ const Register = () => {
        <Link to='/ss9/register'>Register Now!!</Link>
      </button> */}
      <button className='border-primary text-white border-2 px-12 py-6 text-base rounded-[1rem] mt-12 md:hover:text-[2rem] md:hover:p-8 duration-300 font-Goldman hover:bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-500 hover:border-0'>
-       <a href='/ss9/public/anurag.html'>Register Now!!</a>
+       <a href='https://www.csedvit.com/startupstreet9.0/'>Register Now!!</a>
      </button>
      <div className='h-[20rem] w-[20rem] bg-secondary rounded-full blur-[100rem] absolute -z-10 -left-48'></div>
      </div>
