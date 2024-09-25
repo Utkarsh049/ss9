b2993ef9dae65640ccf1a48abd03e931ab158028
Utkarsh049 <utkarshpcs26@gmail.com>
2024-09-25 19:14:17 +0530

    removed choose

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Choose from './components/Choose'
 
 
-const router=createBrowserRouter([
-  {
-    path:'/ss9/',
-    element:<App/>,
-    errorElement:<div>Not Found</div>
-  },
-  {
-    path:'/ss9/register',
-    element:<Choose/>,
-    errorElement:<div>Not Found</div>
-  },
-])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </StrictMode>,
 )
